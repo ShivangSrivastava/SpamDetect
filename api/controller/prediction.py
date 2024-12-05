50b5699dd16827895d6b98032361fe9adbf2ad8a
ShivangSrivastava <shivangsrivastava157@gmail.com>
2024-12-05 08:53:19 +0530

    format backend

--- api/controller/prediction.py
+++ api/controller/prediction.py
@@ -3,23 +3,10 @@ import re
 import joblib
 from sklearn.feature_extraction.text import CountVectorizer
 from model.model import InputData, OutputData
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-
 
 model = joblib.load("../model/model.joblib")
 
 vectorizer =joblib.load("../model/vectorizer.joblib")
-
-stop_words = set(stopwords.words('english'))
-
-# def preprocess_text(text: str) -> str:
-#     text = text.lower()
-#     text = re.sub(r'[^a-z\s]', '', text)
-#     tokens = word_tokenize(text)
-#     tokens = [word for word in tokens if word not in stop_words]
-#     preprocessed_text = " ".join(tokens)
-#     return preprocessed_text
 
 def predict(data: InputData) -> OutputData:
     try:
